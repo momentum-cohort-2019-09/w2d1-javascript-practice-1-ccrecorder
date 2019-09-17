210a376b7113b1c982c0ad972438e5c96f85340b
Charles Wines <ccrecorder@gmail.com>
2019-09-17 10:12:03 -0400

    final

--- homework.js
+++ homework.js
@@ -107,24 +107,24 @@ function multigreeting(name, langCode) {
 // divisor using the instructions above.
 
 function gcd(a, b) {
-	d = 0;
+	let d = 0;
 
-	while (a % 2 === 0 && b % 2 === 0) {
+	while (isEven(a) && isEven(b)) {
 		a = a / 2;
 		b = b / 2;
 		d++;
-
-		while (a != b)
-			if (a % 2 === 0) {
-				a = a / 2;
-			} else if (b % 2 === 0) {
-				b = b / 2;
-			} else if (a > b) {
-				a = (a - b) / 2;
-			} else {
-				b = (b - a) / 2;
-			}
 	}
-
-	return a * 2 ** d;
+	while (a !== b) {
+		if (isEven(a)) {
+			a = a / 2;
+		} else if (isEven(b)) {
+			b = b / 2;
+		} else if (a > b) {
+			a = (a - b) / 2;
+		} else {
+			b = (b - a) / 2;
+		}
+	}
+	let g = a;
+	return g * 2 ** d;
 }
